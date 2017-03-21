cd188f7a27f2a082af6610c5843664ff5464a5fc
happylrd <happylrd17@gmail.com>
2017-03-21 22:36:54 +0800

    Add form validation

--- YouKeOnline/apps/users/views.py
+++ YouKeOnline/apps/users/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
 from .models import UserProfile
+from .forms import LoginForm
 
 
 class CustomBackend(ModelBackend):
@@ -17,15 +19,20 @@ class CustomBackend(ModelBackend):
             return None
 
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return render(request, 'index.html')
-        else:
-            return render(request, 'login.html', {'msg': '用户名或密码错误!'})
-    elif request.method == 'GET':
+class LoginView(View):
+    def get(self, request):
         return render(request, 'login.html', {})
+
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = request.POST.get('username', '')
+            password = request.POST.get('password', '')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return render(request, 'index.html')
+            else:
+                return render(request, 'login.html', {'msg': '用户名或密码错误!'})
+        else:
+            return render(request, 'login.html', {'login_form': login_form})
