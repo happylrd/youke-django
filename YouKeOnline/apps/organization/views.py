3435e4655a8a8acbdec6b514c186ba53edbdf201
happylrd <happylrd17@gmail.com>
2017-04-24 21:18:05 +0800

    Add the filter function

--- YouKeOnline/apps/organization/views.py
+++ YouKeOnline/apps/organization/views.py
@@ -9,9 +9,21 @@ from .models import CourseOrg, CityDict
 class OrgView(View):
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
-        org_nums = all_orgs.count()
+        hot_orgs = all_orgs.order_by('-click_nums')[:3]
 
         all_cities = CityDict.objects.all()
+
+        # 城市筛选
+        city_id = request.GET.get('city', '')
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 类别筛选
+        category = request.GET.get('ct', '')
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        org_nums = all_orgs.count()
 
         # 对课程机构进行分页
         try:
@@ -26,5 +38,8 @@ class OrgView(View):
         return render(request, 'org-list.html', {
             'all_orgs': orgs,
             'all_cities': all_cities,
-            'org_nums': org_nums
+            'org_nums': org_nums,
+            'city_id': city_id,
+            'category': category,
+            'hot_orgs': hot_orgs
         })
