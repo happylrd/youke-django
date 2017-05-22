d3e5e060cf99a84f556fdb9241623448a70a4d49
happylrd <happylrd17@gmail.com>
2017-05-22 10:15:15 +0800

    Add course list page

---
+++ YouKeOnline/apps/courses/urls.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from django.conf.urls import url, include
+
+from .views import CourseListView
+
+urlpatterns = [
+    # 课程列表页
+    url(r'^list/$', CourseListView.as_view(), name='course_list'),
+]
